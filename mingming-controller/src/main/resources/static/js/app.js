6c0c32f644e06c65a66ebbb2b9384e5cb447cd12
guidowb <guidowb@gmail.com>
2015-08-10 13:48:36 -0500

    Canary dashboard now uses notifications

--- mingming-controller/src/main/resources/static/js/app.js
+++ mingming-controller/src/main/resources/static/js/app.js
@@ -10,81 +10,45 @@ angular.module('mingming', [ 'ngRoute', 'ngResource', 'ngAnimate', 'angular.filt
 				controller : 'workers'
 			});
 
-}).run(function($rootScope, $window, $interval) {
-	$window.onfocus = function() {
-		if (!$rootScope.updater) return;
-		if ($rootScope.updaterPromise) return;
-		$rootScope.updater();
-		$rootScope.updaterPromise = $interval($rootScope.updater, 1000)
-		$rootScope.updating = true;
-		$rootScope.$apply();
-	};
-	$window.onblur = function() {
-		if ($rootScope.updaterPromise) {
-			$interval.cancel($rootScope.updaterPromise);
-			$rootScope.updaterPromise = null
-		}
-		$rootScope.updating = false;
-		$rootScope.$apply();
-	};
-	$window.focus();
-	$rootScope.updating = true;
-	$rootScope.autoUpdate = function(updater) {
-		if ($rootScope.updaterPromise) {
-			$interval.cancel($rootScope.updaterPromise);
-			$rootScope.updaterPromise = null
-		}
-		$rootScope.updater = updater;
-		if (!$rootScope.updater) return;
-		$rootScope.updater();
-		$rootScope.updaterPromise = $interval($rootScope.updater, 1000)
-	}
 }).controller('home', function() {
-}).controller('workers', function($rootScope, $scope, $http, $animate) {
+}).controller('workers', function($rootScope, $scope, $http, $timeout, $animate) {
 
-	$rootScope.autoUpdate(function() {
-		$http.get('/workers').success(function(data) {
-			if (!$scope.workers) {
-				$scope.workers = data;
-				return;
-			}
-			var newWorkers = data;
-			var workerIndex = {}
-			for (var i = 0; i < $scope.workers.length; i++) {
-				workerIndex[$scope.workers[i].instanceId] = i;
-			}
-			for (var i = 0; i < data.length; i++) {
-				var index = workerIndex[data[i].instanceId]
-				if (index) {
-//					for (var property in data[i]) {
-//						$scope.workers[index][property] = data[i][property];
-//					}
-					$scope.workers[index].secondsSinceUpdate = data[i].secondsSinceUpdate;
-					$scope.workers[index].secondsSinceCreation = data[i].secondsSinceCreation;
-					delete workerIndex[data[i].instanceId];
+	$scope.timestamp = 0;
+	$scope.backoff = 50;
+	$scope.workers=[];
+	$scope.listen = function() {
+		$http.get('/workers/events?since=' + $scope.timestamp).success(function(notification) {
+			$scope.backoff = 50;
+			$scope.timestamp = notification.timestamp;
+			for (var e = 0; e < notification.events.length; e++) {
+				var event = notification.events[e];
+				if (event.eventType == "refresh") {
+					$scope.workers = event.workers;
 				}
-				else {
-					$scope.workers.push(data[i]);
-				}
-			}
-			for (var index in workerIndex) {
-				for (var i = 0; i < $scope.workers.length; i++) {
-					if ($scope.workers[i].instanceId == index) {
-						$scope.workers.splice(i, 1);
-						break;
+				else if (event.eventType == "update") {
+					var workerIndex = {};
+					for (var i = 0; i < $scope.workers.length; i++) workerIndex[$scope.workers[i].instanceId] = i;
+					for (var w = 0; w < event.workers.length; w++) {
+						var worker = event.workers[w];
+						var index = workerIndex[worker.instanceId];
+						if (typeof index != 'undefined') {
+							for (var property in worker) $scope.workers[index][property] = worker[property];
+						}
+						else {
+							$scope.workers.push(worker);
+							workerIndex[worker.InstanceId] = $scope.workers.length - 1;
+						}
 					}
 				}
 			}
+			$scope.listen();
 		}).error(function() {
-			$scope.workers = []
-		})
-	});
-	$scope.canaryClass = function(canary) {
-		if (canary.secondsSinceCreation < 2) return "gone";
-		if (canary.secondsSinceCreation < 4) return "new";
-		if (canary.secondsSinceUpdate > 26) return "gone";
-		if (canary.secondsSinceUpdate > 16) return "dead";
-		if (canary.secondsSinceUpdate > 6) return "late";
-		return "healthy";
+			if ($scope.backoff < 20000) $scope.backoff = $scope.backoff * 2;
+			$timeout($scope.listen, $scope.backoff);
+		});
 	}
+	$scope.canaryClass = function(canary) {
+		return canary.instanceState;
+	}
+	$scope.listen();
 });
