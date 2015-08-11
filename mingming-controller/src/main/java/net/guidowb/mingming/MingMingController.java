069ce36db131967ebf31e9684340fd4f518c54db
guidowb <guidowb@gmail.com>
2015-08-11 10:08:55 -0500

    Refactored fakes to exercise complete REST path
    This prevents fakes from bypassing the change notification path. It also
    allows them to co-exist with real canaries.
    
    Fixes #3.

--- mingming-controller/src/main/java/net/guidowb/mingming/MingMingController.java
+++ mingming-controller/src/main/java/net/guidowb/mingming/MingMingController.java
@@ -1,14 +1,31 @@
 package net.guidowb.mingming;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import net.guidowb.mingming.controllers.FakeWorkers;
 
 @SpringBootApplication
 @EnableScheduling
-public class MingMingController {
+public class MingMingController implements CommandLineRunner {
+
+	@Autowired private Environment env;
 
 	public static void main(String[] args) {
         SpringApplication.run(MingMingController.class, args);
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+	    String serverPort = env.getProperty("SERVER_PORT", "8080");
+	    String serverHost = env.getProperty("vcap.application.uris[0]", "localhost:" + serverPort);
+	    URI serverURI = URI.create("http://" + serverHost);
+		if (env.containsProperty("FAKEWORKERS")) FakeWorkers.create(serverURI, 10);
+	}
 }
--- mingming-controller/src/main/java/net/guidowb/mingming/controllers/FakeWorkers.java
+++ mingming-controller/src/main/java/net/guidowb/mingming/controllers/FakeWorkers.java
@@ -1,5 +1,6 @@
 package net.guidowb.mingming.controllers;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -9,25 +10,43 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.mock.env.MockEnvironment;
+import org.springframework.web.client.RestTemplate;
 
 import net.guidowb.mingming.model.WorkerInfo;
 
 public class FakeWorkers {
 
-	private List<WorkerInfo> workers = null;
-	private static FakeWorkers instance = null;
 	private int count;
+	private List<WorkerInfo> workers = null;
 	private Set<Integer> dyingWorkers = new HashSet<Integer>();
 	private Set<Integer> deadWorkers = new HashSet<Integer>();
 	private ScheduledExecutorService updatePool = Executors.newScheduledThreadPool(1);
+	private RestTemplate controller = new RestTemplate();
+	private URI controllerURI;
 
-	private FakeWorkers(int count) { this.count = count; }
+	private FakeWorkers(URI controller, int count) {
+		this.controllerURI = controller;
+		this.count = count;
+	}
+
+	private void start() {
+		workers = new ArrayList<WorkerInfo>();
+		for (int index = 0; index < count; index++) {
+			workers.add(createWorker(index));
+		}
+		updatePool.scheduleAtFixedRate(new Runnable() {
+
+			@Override
+			public void run() { updateWorkers(); }
+			
+		}, 1, 1, TimeUnit.SECONDS);
+	}
 
 	private WorkerInfo createWorker(int instance) {
 		MockEnvironment env = new MockEnvironment()
 				.withProperty("vcap.application.uris[0]", "Fake Canaries")
 				.withProperty("vcap.application.space_name", "dynamic test data set")
-				.withProperty("vcap.application.application_name", "fake")
+				.withProperty("vcap.application.application_name", "fake-canary")
 				.withProperty("vcap.application.instance_index", Integer.toString(instance));
 		WorkerInfo worker = new WorkerInfo(env);
 		worker.setLastUpdate();
@@ -36,54 +55,58 @@ public class FakeWorkers {
 
 	private synchronized void updateWorkers() {
 		Set<WorkerInfo> toDelete = new HashSet<WorkerInfo>();
-		if (workers == null) {
-			workers = new ArrayList<WorkerInfo>();
-			for (int index = 0; index < count; index++) {
-				workers.add(createWorker(index));
-			}
-		}
+		
+		// Send updates for workers that are not dying (if it's been more than 4 seconds since the last update)
+		// Mark ones that have been dying for more than 30 seconds for deletion (actual deletion happens outside this loop)
 		for (WorkerInfo worker : workers) {
 			int index = worker.getInstanceIndex();
 			if (dyingWorkers.contains(index)) {
 				if (worker.secondsSinceUpdate() > 30) toDelete.add(worker);	
 			}
-			else worker.setLastUpdate();
+			else {
+				if (worker.secondsSinceUpdate() > 4) {
+					URI workerURI = controllerURI.resolve("/workers/").resolve(worker.getId());
+					worker.setLastUpdate();
+					try { controller.put(workerURI, worker); }
+					catch (Throwable t) {}
+				}
+			}
 		}
+		
+		// Delete the workers that were marked for deletion
+		// Add those workers to the dead list so that we know which ones to revive
 		for (WorkerInfo worker : toDelete) {
 			int index = worker.getInstanceIndex();
 			workers.remove(worker);
 			deadWorkers.add(index);
 			dyingWorkers.remove(index);
 		}
-		if (Math.random() < 0.1) {
+		
+		// With low odds, mark random workers as dying
+		// Doing this in a loop allows for multiple deaths in the same iteration
+		while (Math.random() < 0.1) {
 			dyingWorkers.add((int) Math.floor(Math.random() * count));
 		}
-		if (!deadWorkers.isEmpty() && Math.random() < 0.2) {
+		
+		// If we have dying workers, with low odds, revive one
+		// This is different than replacing, as the instance id remains the same
+		if (!dyingWorkers.isEmpty() && Math.random() < 0.1) {
+			int index = (int) Math.floor(Math.random() * dyingWorkers.size());
+			dyingWorkers.remove(index);
+		}
+
+		// If we have dead workers, after a random interval, replace one with a new instance
+		// Doing this in a loop allows for multiple replacements in the same iteration
+		while (!deadWorkers.isEmpty() && Math.random() < 0.2) {
 			int index = (int) Math.floor(Math.random() * deadWorkers.size());
 			index = (int) deadWorkers.toArray()[index];
 			deadWorkers.remove(index);
 			workers.add(createWorker(index));
 		}
 	}
-
-	private synchronized Iterable<WorkerInfo> listWorkers() {
-		if (workers == null) {
-			workers = new ArrayList<WorkerInfo>();
-			for (int index = 0; index < count; index++) {
-				workers.add(createWorker(index));
-			}
-			updatePool.scheduleAtFixedRate(new Runnable() {
-
-				@Override
-				public void run() { updateWorkers(); }
-				
-			}, 1, 1, TimeUnit.SECONDS);
-		}
-		return workers;
-	}
-
-	public static Iterable<WorkerInfo> getWorkers() {
-		if (instance == null) instance = new FakeWorkers(10);
-		return instance.listWorkers();
+	
+	public static void create(URI controller, int count) {
+		FakeWorkers fakeWorkers = new FakeWorkers(controller, count);
+		fakeWorkers.start();
 	}
 }
