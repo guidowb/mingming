bc2743b81aa85fcf0b178a7b8ca53b755c22d5cb
guidowb <guidowb@gmail.com>
2016-01-05 17:00:06 -0600

    Cosmetic changes

--- canaries/java/Canary.java
+++ canaries/java/Canary.java
@@ -15,28 +15,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 @EnableAutoConfiguration
 public class Canary implements CommandLineRunner {
 
-	public class Payload {
-		public String deploymentName;
-		public String payloadType;
-		public Integer canaryInstance;
-		public String payload;
-
-		public Payload(String payloadType, Object payload) {
-			try { 
-				ObjectMapper mapper = new ObjectMapper();
-				HashMap<String, Object> vcap_application = mapper.readValue(System.getenv("VCAP_APPLICATION"), new TypeReference<Map<String, Object>>(){});
-				System.out.println(System.getenv("VCAP_APPLICATION"));
-				System.out.println(vcap_application.get("uris"));
-				ArrayList<String> application_uris = (ArrayList<String>) vcap_application.get("uris");
-				this.deploymentName = application_uris.get(0);
-				this.canaryInstance = Integer.valueOf(vcap_application.get("instance_index").toString());
-				this.payloadType = payloadType;
-				this.payload = new ObjectMapper().writeValueAsString(payload);
-			}
-			catch (Exception ex) { throw new RuntimeException(ex); }
-		}
-	}
-
 	public static void main(String[] args) throws Exception {
         SpringApplication.run(Canary.class, args);
     }
@@ -49,6 +27,26 @@ public class Canary implements CommandLineRunner {
 	public void postEnvironment(URI uri) {
 		RestTemplate controller = new RestTemplate();
 		Payload payload = new Payload("env", System.getenv());
-		controller.postForLocation(uri, payload);
+		controller.postForLocation(uri.resolve("/payloads"), payload);
+	}
+
+	public class Payload {
+		public String deploymentName;
+		public String payloadType;
+		public Integer canaryInstance;
+		public String payload;
+
+		public Payload(String payloadType, Object payload) {
+			try { 
+				ObjectMapper mapper = new ObjectMapper();
+				HashMap<String, Object> vcap_application = mapper.readValue(System.getenv("VCAP_APPLICATION"), new TypeReference<Map<String, Object>>(){});
+				ArrayList<String> application_uris = (ArrayList<String>) vcap_application.get("uris");
+				this.deploymentName = application_uris.get(0);
+				this.canaryInstance = Integer.valueOf(vcap_application.get("instance_index").toString());
+				this.payloadType = payloadType;
+				this.payload = new ObjectMapper().writeValueAsString(payload);
+			}
+			catch (Exception ex) { throw new RuntimeException(ex); }
+		}
 	}
 }
